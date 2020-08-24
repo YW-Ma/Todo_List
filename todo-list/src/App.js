edcea19cad0e7dba5dbc3e162750be516e197762
WHU-Traffic-Sign-Detection-Team <Ma_Yaowei@outlook.com>
2020-08-24 16:07:34 +0800

    add todo form component

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import './App.css'
+
+function TodoForm(){
+  // 组件1：表单（输入）的组件。
+  return (
+    <input className="input is-info" type="text" placeholder="Text input"></input>
+  );
+}
 
 function App() {
   return (
-    <div></div>
+    <div className="app"> {/* 整个App的容器 */}
+      <div className="todo-list"> {/* todo-list的容器 */}
+        <TodoForm/>
+      </div>
+    </div>
   );
 }
 
